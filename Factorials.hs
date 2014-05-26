9fecbbd31eeee070c4985999b08b84cbd5577f07
R-Morgan <rmorgan@trystero.is>
2014-05-25 21:41:56 -0700

    Change type Int to Integer

--- Factorials.hs
+++ Factorials.hs
@@ -7,12 +7,12 @@ factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
-factorialFall :: Integral -> Integral -> Integral
+factorialFall :: Integer -> Integer -> Integer
 factorialFall x 0 = 1
 factorialFall x 1 = x
 factorialFall x k = (x - k + 1) * factorialFall x (k-1)
 
-factorialRise :: Integral -> Integral -> Integral
+factorialRise :: Integer -> Integer -> Integer
 factorialRise x 0 = 1
 factorialRise x 1 = x
 factorialRise x k = (x + k - 1) * factorialRise x (k-1)
