78667195ec4b3beeaf8811a033c0b5677d2e5dd1
R-Morgan <rmorgan@trystero.is>
2014-05-25 23:09:25 -0700

    Alters types to accommodate large Integer values.
    The functions ran into limitations dealing with calculating the
    binomial coefficet for k = 49 and n = 80.  By using Integer and
    Fractional class.

--- Binomial.hs
+++ Binomial.hs
@@ -7,14 +7,14 @@ module Binomial
 import Data.List
 import Factorials
 
-binomialCoef :: Integer -> Integer -> Float
+binomialCoef :: (Fractional a) => Integer -> Integer -> a 
 binomialCoef k n    =
     let numerator   = fromIntegral $ factorialFall n k
         denominator = fromIntegral $ factorial k 
     in  numerator / denominator
 
-binomialPDF :: Int -> Int -> Float -> Float
+binomialPDF :: (Num a, Fractional a) => Integer -> Integer -> a -> a 
 binomialPDF k n p   = (binomialCoef k n) * (p^k) * (1 - p)^(n-k)
 
-binomialCMF :: Int -> Int -> Float -> Float
+binomialCMF :: (Num a, Fractional a) => Integer -> Integer -> a -> a 
 binomialCMF k n p   = sum [binomialPDF ks n p | ks <- [0..k-1]]
