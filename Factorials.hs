008ede63c42a16cbc85a980a9f90caeb48ba1eff
R-Morgan <rmorgan@trystero.is>
2014-05-25 17:28:17 -0700

    Add Factorial module

---
+++ Factorials.hs
@@ -0,0 +1,13 @@
+module Factorials
+(factorial
+, factorialFall
+) where
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+factorialFall :: Int -> Int -> Int
+factorialFall x 0 = 1
+factorialFall x 1 = x
+factorialFall x k = (x - k + 1) * factorialFall x (k-1)
