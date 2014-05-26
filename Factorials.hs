31a5eb185b730aff5506fe282d054559828344a8
R-Morgan <rmorgan@trystero.is>
2014-05-25 17:38:38 -0700

    Factorial type tweaking

--- Factorials.hs
+++ Factorials.hs
@@ -7,7 +7,7 @@ factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
-factorialFall :: Int -> Int -> Int
+factorialFall :: Int -> Int -> Int 
 factorialFall x 0 = 1
 factorialFall x 1 = x
 factorialFall x k = (x - k + 1) * factorialFall x (k-1)
