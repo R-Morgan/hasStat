8438f79f34c3a71c74ad4b393a9e8f8ccb2f738e
R-Morgan <rmorgan@trystero.is>
2014-05-26 21:54:36 -0700

    Add Log-Likelihood and LL Recursive Function
    This adds the functionality to compute the log likelihood of a binomial
    distribution with k successes in n trials, with level p probability
    of success.

--- Binomial.hs
+++ Binomial.hs
@@ -2,6 +2,9 @@ module Binomial
 ( binomialCoef
 , binomialPMF
 , binomialCMF
+, tuplePMF
+, binomialLL
+, binomialLLRec
 ) where
 
 import Data.List
@@ -22,3 +25,8 @@ binomialCMF k n p   = sum [binomialPMF ks n p | ks <- [0..k-1]]
 tuplePMF :: (Num a, Fractional a) => (Integer, Integer, a) -> a
 tuplePMF (k, n, p) = binomialPMF k n p
 
+binomialLL :: (Num a, Floating a) => a -> a -> a -> a
+binomialLL k n p = k * log p + (n - k) * log (1-p)
+
+binomialLLRec :: (Num a, Integral a, Floating a) => [a] -> [a] -> [a] -> [a]
+binomialLLRec (k:ks) (n:ns) (p:ps) = binomialLL k n p : binomialLLRec ks ns ps
