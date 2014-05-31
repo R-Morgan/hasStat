bb1bb5fff43a2bd7fe8243982f9ea0f812a17f0a
R-Morgan <rmorgan@trystero.is>
2014-05-30 22:34:22 -0700

    Add genNormPDF function

--- Gaussian.hs
+++ Gaussian.hs
@@ -1,9 +1,13 @@
 module Gaussian
 ( standardNormPDF
+, genNormPDF
 ) where
 
 import Dispersion
 
 standardNormPDF :: (Num a, Floating a) => a -> a
-standardNormPDF x = e**((-1/2) * x^2) / sqrt 2*pi
+standardNormPDF x = e**(-(((1/2) * x^2) / sqrt 2*pi))
     where e = exp(1)
+
+genNormPDF :: (Num a, Floating a) => a -> a -> a -> a
+genNormPDF x mu sigma = (1 / (sigma * sqrt 2*pi)) * exp(-((x - mu)^2 / 2*sigma^2))
