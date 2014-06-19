13ddfc83c3cca49da6ee27cac9d208fedf750fe3
R-Morgan <rmorgan@trystero.is>
2014-06-19 13:31:20 -0700

    Add module code

--- Gamma.hs
+++ Gamma.hs
@@ -1,3 +1,6 @@
+module Gamma
+(gammaFunc) where
+
 gammaFunc :: Float -> Int -> Float
 gammaFunc t n =
     (exp(-gamma*t)/t) * product [(1 + (t/(fromIntegral ns)))**(-1) * (exp(t/(fromIntegral ns))) | ns <- [1..n]]
