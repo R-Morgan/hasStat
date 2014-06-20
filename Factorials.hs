212b9358b10d1728e8a57cc17d3c1fbf0a9c2a94
R-Morgan <rmorgan@trystero.is>
2014-06-20 09:50:49 -0700

    Add Pocahammer Symbol Function

--- Factorials.hs
+++ Factorials.hs
@@ -1,7 +1,10 @@
 module Factorials
 (factorial
 , factorialFall
+, pocahammer
 ) where
+
+import Gamma
 
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
@@ -16,3 +19,8 @@ factorialRise :: Integer -> Integer -> Integer
 factorialRise x 0 = 1
 factorialRise x 1 = x
 factorialRise x k = (x + k - 1) * factorialRise x (k-1)
+
+pocahammer :: Float -> Float -> Float
+pocahammer x 0 = 1
+pocahammer x 1 = x
+pocahammer x n = (gammaFunc (x + n) 10000) / gammaFunc x 10000
