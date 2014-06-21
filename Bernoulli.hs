d6b33cf07f04c0055ffa6859edda36a7c2a7c12c
R-Morgan <rmorgan@trystero.is>
2014-06-20 18:11:17 -0700

    Add Bernoulli Product function with comments

--- Bernoulli.hs
+++ Bernoulli.hs
@@ -5,3 +5,10 @@ module Bernoulli
 bernoulliPMF :: Int -> Float -> Float
 bernoulliPMF k p = p^k * (1-p)^(1-k) 
 
+--Function to calculate product of probabilities for each document which
+--are each labeled either 0 or 1. Elements of the list comprehension are
+--the probability of the element of the list being either 1 or 0, given
+--p, which is the probability of getting a 1 or 0
+
+bernoulliProd :: [Int] -> Float -> Float
+bernoulliProd ks p = product [bernoulliPMF k p | k <- ks] 
