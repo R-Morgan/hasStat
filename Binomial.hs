2504e5e672da77768a1f6409fd1d3b95d77ecfc2
R-Morgan <rmorgan@trystero.is>
2014-06-28 00:00:03 -0700

    Add edge case for binomialLLRec function

--- Binomial.hs
+++ Binomial.hs
@@ -28,5 +28,6 @@ tuplePMF (k, n, p) = binomialPMF k n p
 binomialLL :: (Num a, Floating a) => a -> a -> a -> a
 binomialLL k n p = k * log p + (n - k) * log (1-p)
 
-binomialLLRec :: (Num a, Integral a, Floating a) => [a] -> [a] -> [a] -> [a]
+binomialLLRec :: (Num a, Floating a) => [a] -> [a] -> [a] -> [a]
+binomialLLRec [] [] [] = []
 binomialLLRec (k:ks) (n:ns) (p:ps) = binomialLL k n p : binomialLLRec ks ns ps
