c0f9da422f10b2cf4b24e3670a3db9eccbeb353b
R-Morgan <rmorgan@trystero.is>
2014-06-29 00:26:52 -0700

    Add doAndGlue function.
    This function is intended in aiding in the sampling of conditional
    distributions in a gibs sampler. Still having trouble implementing
    the iterative part.

--- StatUtils.hs
+++ StatUtils.hs
@@ -7,3 +7,9 @@ dropKth 0 xs = xs
 dropKth 1 xs = tail xs
 dropKth n xs = let (a, b) = splitAt n xs
                in init a ++ b
+               
+doAndGlue :: Int -> ([a] -> a) ->  [a] -> [a]
+doAndGlue 0 f xs = xs
+doAndGlue 1 f xs = hd ++ [f hd] where hd = init xs
+-- doAndGlue n f xs = 
+-- ++ t where t = tail xs
