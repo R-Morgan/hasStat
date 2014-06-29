9e3997219b1966f585f81aac3cf07b2e0b81ec88
R-Morgan <rmorgan@trystero.is>
2014-06-29 13:30:53 -0700

    Fix doAndGlue Function.
    The doAndGlue Function is a function that should be useful in a
    Gibbs sampler, because it can apply a function to overr a list
    to use subsequent values to calculate the first new value, then
    incorporate that new value into the calculation for the next
    element of the list.

--- StatUtils.hs
+++ StatUtils.hs
@@ -1,5 +1,6 @@
 module StatUtils
-(dropKth
+( dropKth
+, doAndGlue
 ) where
 
 dropKth :: Int -> [a] -> [a]
@@ -9,7 +10,17 @@ dropKth n xs = let (a, b) = splitAt n xs
                in init a ++ b
                
 doAndGlue :: Int -> ([a] -> a) ->  [a] -> [a]
-doAndGlue 0 f xs = xs
-doAndGlue 1 f xs = hd ++ [f hd] where hd = init xs
--- doAndGlue n f xs = 
--- ++ t where t = tail xs
+doAndGlue _ _ [] = []
+doAndGlue 1 f xs = let tmpList = tail xs
+                       tmpVal  = f tmpList -- scalar
+                       reRunList = [tmpVal] ++ tmpList
+                   in  doAndGlue (1 + 1) f reRunList
+doAndGlue n f xs
+    | n > 1 && n < length xs = let tmpList = dropKth n xs
+                                   tmpVal  = f tmpList
+                                   (aList, bList)  = splitAt n xs
+                                   reRunList =  (init aList) ++ [tmpVal] ++ bList
+                               in doAndGlue (n + 1) f reRunList
+    | n == length xs = init xs ++ [f $ init xs]
+    | otherwise = doAndGlue (n + 1) f [] 
+
