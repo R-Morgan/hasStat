ba343cfac0e58039024b2df9b48377bf832593d7
R-Morgan <rmorgan@trystero.is>
2014-06-30 10:40:13 -0700

    Fix typeclass restrictions on gammaFunc and add gammaPDF

--- Gamma.hs
+++ Gamma.hs
@@ -1,7 +1,17 @@
 module Gamma
-(gammaFunc) where
+(
+gammaFunc
+, gammaPDF
+) where
 
-gammaFunc :: Float -> Int -> Float
+gammaFunc :: (Num a, Floating a) => a -> Int -> a
 gammaFunc t n =
     (exp(-gamma*t)/t) * product [(1 + (t/(fromIntegral ns)))**(-1) * (exp(t/(fromIntegral ns))) | ns <- [1..n]]
     where gamma = 0.577216
+
+gammaPDF :: (Num a, Floating a) => a -> a -> a -> a
+gammaPDF x k theta = let gam       = gammaFunc k 10000
+                         numerator =(x ** (k-1)) * exp (-(x / theta))
+                         denominator = (theta ** k) * gam
+                     in  numerator / denominator
+
