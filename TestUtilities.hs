fae80bb3fc3da150c23c4e6d272c9ca548ea1b7d
R-Morgan <rmorgan@trystero.is>
2014-06-30 09:55:07 -0700

    Add test data vectors

--- TestUtilities.hs
+++ TestUtilities.hs
@@ -1,3 +1,15 @@
+module TestUtilities
+( sequencer 
+, tmpA
+, tmpB
+) where
+
 sequencer :: Int -> Int -> [Float] 
 sequencer n k = [(fromIntegral x) / (fromIntegral k) | x <- [0..n]]
+
+tmpA :: (Num a, Floating a) => [a]
+tmpA = [0.05, 0.05, 0.1, 0.2, 0.15, 0.25, 0.1, 0.025, 0.025, 0.05] -- proportion of 'words'
+
+tmpB :: (Num a, Floating a) => [a]
+tmpB = [a * b | (a, b) <- zip tmpA (replicate 10 10000)]            -- actual counts
 
