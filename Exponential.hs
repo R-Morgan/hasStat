a8582f83c1316362aa8dbd28a7731e3129cbfb7c
R-Morgan <rmorgan@trystero.is>
2014-07-01 17:55:50 -0700

    Add exponential CDF

--- Exponential.hs
+++ Exponential.hs
@@ -6,3 +6,5 @@ exponentialPDF
 exponentialPDF :: (Num a, Floating a) => a -> a -> a
 exponentialPDF x lambda = lambda * exp (-1 * lambda * x)
 
+exponentialCDF :: (Num a, Floating a) => a -> a -> a
+exponentialCDF x lambda = 1 - exp(-1 * lambda * x)
