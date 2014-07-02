cff386ae3634c535d13acb7471757cda171fb895
R-Morgan <rmorgan@trystero.is>
2014-07-02 08:24:27 -0700

    Add exponential quantile function

--- Exponential.hs
+++ Exponential.hs
@@ -3,8 +3,14 @@ module Exponential
 exponentialPDF
 ) where
 
+import StatUtils
+
 exponentialPDF :: (Num a, Floating a) => a -> a -> a
 exponentialPDF x lambda = lambda * exp (-1 * lambda * x)
 
 exponentialCDF :: (Num a, Floating a) => a -> a -> a
 exponentialCDF x lambda = 1 - exp(-1 * lambda * x)
+
+exponentialQuantile :: (Num a, Floating a) => a -> a -> a
+exponentialQuantile x lambda = (-1 * log (1-x)) / lambda 
+
