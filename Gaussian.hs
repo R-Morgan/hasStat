e71d8bb5b8375c1733e535b6fa7da446d8235216
R-Morgan <rmorgan@trystero.is>
2014-07-02 08:40:37 -0700

    Fix standard normal pdf
    The issue seemed to be that sqrt 2 * pi was not computing properly.
    Added parens, so that 2 * pi would evaluate first, rather than
    what was happening. Seems like there was an order of operations
    issue here.

--- Gaussian.hs
+++ Gaussian.hs
@@ -6,8 +6,7 @@ module Gaussian
 import Dispersion
 
 standardNormPDF :: (Num a, Floating a) => a -> a
-standardNormPDF x = e**(-(((1/2) * x^2) / sqrt 2*pi))
-    where e = exp(1)
-
+standardNormPDF x = exp (-((1/2) * x^2)) / (sqrt (2 * pi))
+          
 genNormPDF :: (Num a, Floating a) => a -> a -> a -> a
 genNormPDF x mu sigma = (1 / (sigma * sqrt 2*pi)) * exp(-((x - mu)^2 / 2*sigma^2))
