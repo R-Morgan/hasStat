5302fe17bb88afdb166dacda5296a4bfd870c3d8
R-Morgan <rmorgan@trystero.is>
2014-07-02 22:36:40 -0700

    Clean up over long line

--- HyperGeometric.hs
+++ HyperGeometric.hs
@@ -6,5 +6,6 @@ module HyperGeometric
 import Factorials
 
 hyperGeomFirstOrd :: (Num a, Enum a, Floating a, Ord a) => a -> a -> a -> a
-hyperGeomFirstOrd a b x = sum [(pocahammer a k / pocahammer b k) * (x ** k) * (1 / factorial k) | k <- [0..10]]
+hyperGeomFirstOrd a b x = sum [(pocahammer a k / pocahammer b k) * (x ** k) * 
+                               (1 / factorial k) | k <- [0..10]]
 
