7d20c7b7f1bf704ed65f365f6a6596048add8062
R-Morgan <rmorgan@trystero.is>
2014-07-02 22:53:01 -0700

    Remove developmental code from master version

--- Gaussian.hs
+++ Gaussian.hs
@@ -1,9 +1,11 @@
 module Gaussian
 ( standardNormPDF
 , generalNormPDF
+, normGenerator
 ) where
 
 import Dispersion
+import StatUtils
 
 standardNormPDF :: (Num a, Floating a) => a -> a
 standardNormPDF x = exp (-((1/2) * x^2)) / (sqrt (2 * pi))
@@ -12,5 +14,5 @@ generalNormPDF :: (Num a, Floating a) => a -> a -> a -> a
 generalNormPDF x mu sigma = constant * (e ** exponent)
     where constant = (1 / (sigma * sqrt (2 * pi)))
           e        = exp 1
-          exponent = -(((x - mu)**2) / (2 * sigma^2))
+          exponent = -(((x - mu)**2) / (2 * sigma**2))
       
