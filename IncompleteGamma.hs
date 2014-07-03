0608c15c0c69c6819433525b1d4e8e7b6c5abaf1
R-Morgan <rmorgan@trystero.is>
2014-07-02 22:38:17 -0700

    Clean up over long line

--- IncompleteGamma.hs
+++ IncompleteGamma.hs
@@ -6,6 +6,7 @@ gammaLowerIncomplete
 import HyperGeometric
 
 gammaLowerIncomplete :: (Num a, Ord a, Enum a, Floating a) => a -> a -> a
-gammaLowerIncomplete a x = (a ** (-1)) * (x ** a) * exp (-x) * hyperGeomFirstOrd 1 (1 + a) x
+gammaLowerIncomplete a x = (a ** (-1)) * (x ** a) * exp (-x) * 
+                           hyperGeomFirstOrd 1 (1 + a) x
 
 
