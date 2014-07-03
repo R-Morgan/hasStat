d76bed0d68d20b542699d3c6e8334218cd7856a1
R-Morgan <rmorgan@trystero.is>
2014-07-02 22:44:37 -0700

    Clean up over long lines

--- Beta.hs
+++ Beta.hs
@@ -11,16 +11,20 @@ import Factorials
 data BetaParams = BetaParams Float Float deriving(Show)
 
 betaFunc :: BetaParams -> Float
-betaFunc (BetaParams alpha beta) = ((gammaFunc alpha 10000) * (gammaFunc beta 10000)) / gammaFunc (alpha + beta) 10000
+betaFunc (BetaParams alpha beta) = ((gammaFunc alpha 10000) * 
+                                   (gammaFunc beta 10000)) / gammaFunc (alpha + beta) 10000
 
 betaPDF :: Float ->  BetaParams -> Float
-betaPDF x (BetaParams alpha beta) = (1/(betaFunc (BetaParams alpha beta))) * x**(alpha - 1) * (1-x)**(beta - 1)
+betaPDF x (BetaParams alpha beta) = (1/(betaFunc (BetaParams alpha beta))) * 
+                                    x ** (alpha - 1) * (1-x) ** (beta - 1)
 
 incompleteBeta :: Float -> BetaParams -> Float
 incompleteBeta x (BetaParams alpha beta) =
-   x**alpha * sum [((pocahammer (1-beta) (fromIntegral n)) / ((fromIntegral (factorial n)) * (alpha + (fromIntegral n)))) * x^n | n <- [0..20]] 
+   x**alpha * sum [((pocahammer (1-beta) (fromIntegral n)) / 
+                   ((fromIntegral (factorial n)) * 
+                   (alpha + (fromIntegral n)))) * x^n | n <- [0..20]] 
 
 multinomialBeta :: [Float] -> Float
 multinomialBeta x = numerator / denominator
-    where numerator = product  [gammaFunc alpha 10000 | alpha <- x] 
+    where numerator   = product [gammaFunc alpha 10000 | alpha <- x] 
           denominator = gammaFunc (sum [alpha | alpha <- x]) 10000
