fb6f51aad9a2ceaadd6d1a9b879f530d7c45a4ac
R-Morgan <rmorgan@trystero.is>
2014-07-03 20:42:40 -0700

    Add normal variate generator

--- Gaussian.hs
+++ Gaussian.hs
@@ -16,3 +16,9 @@ generalNormPDF x mu sigma = constant * (e ** exponent)
           e        = exp 1
           exponent = -(((x - mu)**2) / (2 * sigma**2))
       
+normGenerator :: (Num a, Floating a) =>  Int -> Int -> (a, a)
+normGenerator seedA seedB = (x, y) 
+    --let u = log $ lcValGen seedA * (-2)
+    where x = sqrt ((-2) * (log $ lcValGen seedA)) * cos (2 * pi * (lcValGen seedB))
+          y = sqrt ((-2) * (log $ lcValGen seedA)) * sin (2 * pi * (lcValGen seedB))
+    --in x : y
