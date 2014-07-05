109155e03fe147042c2160cf2f88ab1c8e933807
R-Morgan <rmorgan@trystero.is>
2014-07-04 20:37:02 -0700

    Add cummulative mass function

--- Poisson.hs
+++ Poisson.hs
@@ -2,6 +2,7 @@ module Poisson
 ( poissonPMF
 ) where
 
+import IncompleteGamma
 import Factorials
 
 poissonPMF :: Float -> Int -> Float
@@ -9,3 +10,7 @@ poissonPMF k lambda    = (numerator / denominator) * exp ((-1) * (fromIntegral l
     where  numerator   = fromIntegral lambda ** k 
            denominator = factorial k
 
+poissonCMF :: Float -> Int -> Float
+poissonCMF k lambda =  numerator / denominator
+    where numerator = gammaLowerIncomplete (fromIntegral (truncate (k + 1))) (fromIntegral lambda)
+          denominator = factorial (fromIntegral (truncate k))
