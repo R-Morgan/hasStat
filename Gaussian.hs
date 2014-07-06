6bb1ce9342423fab3571fa4b71f51429c86e1800
R-Morgan <rmorgan@trystero.is>
2014-07-06 13:54:03 -0700

    Add Eq a to typeclass constratin in normGenerator

--- Gaussian.hs
+++ Gaussian.hs
@@ -10,13 +10,14 @@ import StatUtils
 standardNormPDF :: (Num a, Floating a) => a -> a
 standardNormPDF x = exp (-((1/2) * x^2)) / (sqrt (2 * pi))
           
-generalNormPDF :: (Num a, Floating a) => a -> a -> a -> a
+generalNormPDF :: (Num a, Floating a, Eq a) => a -> a -> a -> a
+generalNormPDF x mu 0     = 0
 generalNormPDF x mu sigma = constant * (e ** exponent)
     where constant = (1 / (sigma * sqrt (2 * pi)))
           e        = exp 1
           exponent = -(((x - mu)**2) / (2 * sigma**2))
       
-normGenerator :: (Num a, Floating a) =>  Int -> Int -> (a, a)
+normGenerator :: (Num a, Floating a, Eq a) =>  Int -> Int -> (a, a)
 normGenerator seedA seedB = (x, y) 
     where x = sqrt ((-2) * (log $ lcValGen seedA)) * cos (2 * pi * (lcValGen seedB))
           y = sqrt ((-2) * (log $ lcValGen seedA)) * sin (2 * pi * (lcValGen seedB))
