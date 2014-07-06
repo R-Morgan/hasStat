11bbf13f6b933665b85774baf6fed54d294f7567
R-Morgan <rmorgan@trystero.is>
2014-07-05 18:15:21 -0700

    Add alternative PDF

--- Exponential.hs
+++ Exponential.hs
@@ -1,5 +1,6 @@
 module Exponential
 ( exponentialPDF
+, exponentialAltPDF
 , exponentialCDF
 , exponentialQuantile
 , exponentialValGen
@@ -13,6 +14,12 @@ exponentialPDF x lambda
     | lambda <= 0 = 0 
     | otherwise = lambda * exp (-1 * lambda * x)
 
+exponentialAltPDF :: (Num a, Ord a, Floating a) => a -> a -> a
+exponentialAltPDF x beta
+    | x      <= 0 = 0
+    | beta   <= 0 = 0 
+    | otherwise = (1 / beta) * exp (-1 * (x / beta))
+
 exponentialCDF :: (Num a, Floating a) => a -> a -> a
 exponentialCDF x lambda = 1 - exp(-1 * lambda * x)
 
@@ -21,5 +28,4 @@ exponentialQuantile x lambda = (-1 * log (1-x)) / lambda
 
 exponentialValGen :: (Num a, Floating a) => a -> a -> a
 exponentialValGen u lambda = exponentialQuantile u lambda  -- u ~ Unif(0, 1)
-
 
