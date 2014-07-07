b3b13d26c8f76b69173a8edd66efd8a1a4970dca
R-Morgan <rmorgan@trystero.is>
2014-07-06 21:19:54 -0700

    Code clean up and Add CDF function
    Restructured the incomplete beta function to work with some updates
    to the Factorial module. Also added a CDF. Unfortunately, the beta
    lower incomplete function is not evaluating well for all possible
    values of the parameters, so this code is not ready yet.

--- Beta.hs
+++ Beta.hs
@@ -2,28 +2,56 @@ module Beta
 ( betaFunc
 , betaPDF
 , incompleteBeta
+--, betaCDF
 , multinomialBeta
 ) where 
 
 import Gamma
 import Factorials
 
+-- Type declaration for BetaParams (alpha and beta). Not sure if this is the
+-- best use of the typing system or if it really gets me anything.
+-- 
+-- Parameters:
+--            alpha : continuous shape parameter on interval (0,1)
+--            beta  : continuous shape parameter 
 data BetaParams = BetaParams Float Float deriving(Show)
 
+-- Beta function implemented via ratio of gamma functions. Used in computation
+-- of the Beta Distribution's PDF.
 betaFunc :: BetaParams -> Float
-betaFunc (BetaParams alpha beta) = ((gammaFunc alpha 10000) * 
-                                   (gammaFunc beta 10000)) / gammaFunc (alpha + beta) 10000
+betaFunc (BetaParams alpha beta) = 
+    ((gammaFunc alpha 10000) * (gammaFunc beta 10000)) / gammaFunc (alpha + beta) 10000
 
+-- Beta  probability distribution function
+-- Parameters:
+--            x : Value IV at which to calculate density 
 betaPDF :: Float ->  BetaParams -> Float
 betaPDF x (BetaParams alpha beta) = (1/(betaFunc (BetaParams alpha beta))) * 
                                     x ** (alpha - 1) * (1-x) ** (beta - 1)
 
+-- Incomplete beta function, used in computation of the cdf 
+-- Parameters:
+--            x : Value IV at which to calculate cumulative density, given
+--                BetaParams
 incompleteBeta :: Float -> BetaParams -> Float
 incompleteBeta x (BetaParams alpha beta) =
-   x**alpha * sum [((pocahammer (1-beta) (fromIntegral n)) / 
-                   ((fromIntegral (factorial n)) * 
-                   (alpha + (fromIntegral n)))) * x^n | n <- [0..20]] 
+    constant * sum [(pocahammer (1 - beta) n) / ((factorial n) * (alpha + n)) * 
+                    x ** n | n <- [0..20]] where constant  = (x ** alpha)
 
+-- BetaCDF or Regularized incomplete beta function.
+-- Parameters:
+--            x : Value IV at which to calculate cumulative density, given
+--                BetaParams
+
+betaCDF :: Float -> BetaParams -> Float
+betaCDF x (BetaParams alpha beta) = numerator / denominator
+    where numerator   = incompleteBeta x (BetaParams alpha beta) 
+          denominator = betaFunc (BetaParams alpha beta)    
+
+-- Multinomial Beta function
+-- Parameters:
+--            x: 
 multinomialBeta :: [Float] -> Float
 multinomialBeta x = numerator / denominator
     where numerator   = product [gammaFunc alpha 10000 | alpha <- x] 
